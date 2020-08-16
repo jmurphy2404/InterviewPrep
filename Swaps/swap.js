3c23a84290d7601df99601607e060c1a558ca95e
John Murphy <murf2073@gmail.com>
2020-08-16 02:42:43 -0400

    updated for more test scenarios to ensure edge case coverage

--- Swaps/swap.js
+++ Swaps/swap.js
@@ -7,7 +7,9 @@ var swap = function(array, firstIndex, secondIndex) {
 var testArray = [7, 9, 4];
 swap(testArray, 0, 1);
 Program.assertEqual(testArray, [9, 7, 4]);
-
-
+swap(testArray, 1, 2);
+Program.assertEqual(testArray, [9, 4, 7]);
+swap(testArray, 0, 2);
+Program.assertEqual(testArray, [7, 4, 9]);
 
 println(testArray);
